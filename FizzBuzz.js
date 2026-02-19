b8be0b47ad7873f19f0b11f4f6af8ae82c510729
jiyamahalavat <jiyamahalavat@gmail.com>
2026-02-19 13:54:21 -0500

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,33 @@
+// TODO: Define constants for the form and the result display area
+const num
+// TODO: Add the first line of the event listener to handle form submission
+
+    // Prevent the form from refreshing the page
+    event.preventDefault();
+
+// TODO: Get the number from the form input
+let num = Number(input.value)
+
+// TODO: Write FizzBuzz logic here
+// If divisible by both 3 and 5, set the output to "FizzBuzz"
+// If divisible by 3, set the output to "Fizz"
+// If divisible by 5, set the output to "Buzz"
+// Otherwise, set the output to the number itself
+
+
+for(let i )
+
+
+if (num % 3);
+    console.log("Fizz")
+    i=i+1
+else if (num % 5);
+    console.log("Buzz")
+
+else if (num % 3 && num % 5);
+    console.log("FizzBuzz")
+else;
+    console.log(num) += i
+
+// Display the result on the page
+result.textContent = output;
