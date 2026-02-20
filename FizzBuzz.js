e024fbf5c52ffed6ea39a8b638ec2efc3c5dcbb8
jiyamahalavat <jiyamahalavat@gmail.com>
2026-02-20 12:51:30 -0500

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,10 @@
 // TODO: Define constants for the form and the result display area
-const num
 // TODO: Add the first line of the event listener to handle form submission
 
-    // Prevent the form from refreshing the page
-    event.preventDefault();
+// Prevent the form from refreshing the page
 
 // TODO: Get the number from the form input
-let num = Number(input.value)
+
 
 // TODO: Write FizzBuzz logic here
 // If divisible by both 3 and 5, set the output to "FizzBuzz"
@@ -15,19 +13,25 @@ let num = Number(input.value)
 // Otherwise, set the output to the number itself
 
 
-for(let i )
+document.getElementById("submitBtn").addEventListener("click", function() {
 
+  const num = Number(document.getElementById("numberInput").value);
+  let result = document.getElementById("result");
 
-if (num % 3);
-    console.log("Fizz")
-    i=i+1
-else if (num % 5);
-    console.log("Buzz")
+  if (isNaN(num)) {
+    result.textContent = "Please enter a valid number.";
+  }
+  else if (num % 3 === 0 && num % 5 === 0) {
+    result.textContent = "FizzBuzz";
+  }
+  else if (num % 3 === 0) {
+    result.textContent = "Fizz";
+  }
+  else if (num % 5 === 0) {
+    result.textContent = "Buzz";
+  }
+  else {
+    result.textContent = num;
+  }
 
-else if (num % 3 && num % 5);
-    console.log("FizzBuzz")
-else;
-    console.log(num) += i
-
-// Display the result on the page
-result.textContent = output;
+});
